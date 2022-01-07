eba3715cfa10891245651e7c1c3a12912748ab89
Nishajha2030 <87373118+Nishajha2030@users.noreply.github.com>
2022-01-07 17:25:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var mouseEvent="empty"
+canvas=document.getElementById("myCanvas")
+ctx=canvas.getContext("2d")
+canvas.addEventListener("mousedown",mymousedown)
+function mymousedown(e){
+    color = document.getElementById("color").value;
+    width_of_line=document.getElementById("width_of_the_line").value;
+    radius=document.getElementById("radius").value;
+    mouseEvent="mouseDown";
+}
+canvas.addEventListener("mousemove",mymousemove)
+function mymousemove(e){
+    current_position_of_mouse_x =e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y =e.clientY - canvas.offsetTop;
+
+    if(mouseEvent == "mouseDown"){
+        //console.log("Current position of x and y coordinates = ");
+        //console.log("x = "+current_position_of_mouse_x+"y = "+ current_position_of_mouse_y)
+        
+        
+        ctx.beginPath();
+        ctx.strokeStyle=color;
+        ctx.lineWidth=width_of_line;
+        ctx.arc(current_position_of_mouse_x,current_position_of_mouse_y,radius,0,Math.PI*2);
+        ctx.stroke();
+    }
+    
+}
+    
